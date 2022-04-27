0469e07e53fe36f46f7ab7526ca89e2f3cda1fcd
cljang <clayton.jang@gmail.com>
2022-04-26 21:23:23 -0700

    Add toggleable navbar to header with functional links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Header() {
+  const [navOpen, setNavOpen] = useState(false)
+
+  const handleNavButton = (e) => {
+    e.preventDefault();
+    setNavOpen(!navOpen)
+  }
+
   return (
-    <header className="app-header">
-      <div className="logo">
-        <h1>ovation.</h1>
+    <header className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">
+          <h1>ovation.</h1>
+        </Link>
       </div>
+      
+      <button
+        className={"navbar-btn" + (navOpen ? " navbar-toggled" : "")}
+        id="btn-menu"
+        aria-label="Navigation Menu"
+        aria-controls="header-menu"
+        aria-expanded={navOpen}
+        onClick={handleNavButton}
+      >
+        <span className="bar" id="bar-1"></span>
+        <span className="bar" id="bar-2"></span>
+        <span className="bar" id="bar-3"></span>
+      </button>
+      <nav className={"navbar-nav" + (navOpen ? " navbar-toggled" : "")}>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/favourites">Favourites</Link></li>
+        </ul>
+      </nav>
     </header>
   );
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import PageHome from "../pages/PageHome";
@@ -8,12 +8,13 @@ import PageFavourite from "../pages/PageFavourite";
 function AppRouter() {
   return (
     <BrowserRouter>
+      <Link to="#site-main" className="screen-reader-text">Skip to content</Link>
       <div className="site-wrapper">
         <Header />
         <Routes>
             <Route path="/" element={<PageHome />} />
             <Route path="/about" element={<PageAbout />} />
-            <Route path="/favourite" element={<PageFavourite />} />
+            <Route path="/favourites" element={<PageFavourite />} />
             <Route path="*" element={<h2>404</h2>} />
         </Routes>
         <Footer />
