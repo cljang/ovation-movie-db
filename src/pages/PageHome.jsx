2ef7a461b2e56355d9f7168bbd253484c1a708d3
cljang <clayton.jang@gmail.com>
2022-04-29 19:19:21 -0700

    Simplify pagehome fetchMovies process

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -12,18 +12,17 @@ const PageHome = () => {
   // Movie List
   const [movieList, setMovieList] = useState(false);
 
-  // Fetch movies 
-  const fetchMovies = useCallback(async () => {
-    const res = await fetch(`${endpointMovieSearch}${selectedMovieFilter}?api_key=${API_KEY}`);
-    const data = await res.json();
-    const selectedMovies = data.results;
-    setMovieList(selectedMovies);
-  },[selectedMovieFilter])
-
   // Re-fetch movies if the selectedMovieFilter changes - this will also occur on page load
   useEffect(() => {
+    const fetchMovies = async () => {
+      const res = await fetch(`${endpointMovieSearch}${selectedMovieFilter}?api_key=${API_KEY}`);
+      const data = await res.json();
+      const selectedMovies = data.results;
+      setMovieList(selectedMovies);
+    }
+
     fetchMovies();
-  }, [selectedMovieFilter, fetchMovies])
+  }, [selectedMovieFilter])
   
 
   return (
