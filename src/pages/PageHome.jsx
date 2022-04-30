bb564585d3cf12c32195c85c4b48363214c86ce1
cljang <clayton.jang@gmail.com>
2022-04-29 21:52:40 -0700

    Truncate movie-overview to 4 lines using css

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react"
+import { useState, useEffect } from "react"
 import { endpointMovieSearch } from "../global/globals"
 import { useSelector } from "react-redux"
 import { API_KEY } from "../global/api-key"
