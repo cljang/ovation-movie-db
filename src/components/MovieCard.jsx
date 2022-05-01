ccc112d52d6d418d6ad61bea3cbeabe9a74837a0
cljang <clayton.jang@gmail.com>
2022-04-30 19:03:38 -0700

    Try to reformat card overview

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { pathToPoster } from "../global/globals";
 
 function MovieCard({movie}) {
@@ -9,15 +10,17 @@ function MovieCard({movie}) {
 
   // On mount, add a resize event listener to change the font size of the card relative to the width of the card, this will ensure that the cards are similarly legible on all screens
   useEffect(() => {
-    console.log("effect");
     const getRelativeFontSize = () => {
       // Select the card and get its width
       const element = document.querySelector(`#${movieCardID}`);
       const elementWidth = element.offsetWidth;
 
       // Take a fraction of the card's width and use it to set the nearest whole font size 
-      // 0.05 is experimentally found to give a font size of 16px at 360px viewport width
-      setFontSize(Math.round(elementWidth*0.054))
+      // 0.054 is experimentally found to give a font size of 15px at 360px viewport width
+      const relativeFontSize = elementWidth*0.05;
+      const roundedFontSize = Math.round(relativeFontSize);
+
+      setFontSize(relativeFontSize)
     }
 
     getRelativeFontSize();
@@ -29,16 +32,37 @@ function MovieCard({movie}) {
     fontSize: `${fontSize}px`
   }
 
-  console.log(fontSize);
+  // Format long overviews to be less than a specified character limit
+  const formatOverview = (str="", characterLimit=200) => {
+    if (str.length > characterLimit) {
+      // Find the index of the last space before the characterLimit (to find last whole word before characterLimit)
+      let lastSpace = str.slice(0,characterLimit).lastIndexOf(" ");
+
+      // Slice front and back half of string around the last space
+      let strFront = str.slice(0, lastSpace);
+      let strBack = str.slice(lastSpace);
+
+      return (
+        <>
+          {strFront}...
+          <span className="hidden">{strBack}</span>
+        </>
+      )
+    } else {
+      return str;
+    }
+  }
 
   return (
     <article id={movieCardID} className="movie-card">
       <img src={`${pathToPoster}${movie.poster_path}`} alt={movie.title} className="movie-card-poster" />
       <div className="movie-card-overlay"  style={style}>
-        <p>{movie.vote_average}</p>
+        <p className="movie-rating">{movie.vote_average}</p>
         <h3 className="movie-title">{movie.title}</h3>
         <p className="movie-release-date">{movie.release_date}</p>
         <p className="movie-overview">{movie.overview}</p>
+        {/* <p className="movie-overview">{formatOverview(movie.overview)}</p> */}
+        <Link to="/" className="movie-info-button">More Info</Link>
       </div>
     </article>
   );
