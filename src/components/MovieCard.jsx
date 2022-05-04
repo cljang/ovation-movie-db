bd99e7e00ccff31faa6b1af7fd79214908c2f4d7
cljang <clayton.jang@gmail.com>
2022-05-03 22:15:46 -0700

    Make movie card flipping toggleable and adjust breakpoints

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { pathToPoster } from "../global/globals";
 import placeholderPoster from "../images/placeholder_poster.png";
@@ -6,11 +7,17 @@ import FavouriteHeart from "./FavouriteHeart";
 
 function MovieCard({movie}) {
 
+  const [ flipped, setFlipped ] = useState(false);
+
+  const flipCard = () => {
+    setFlipped(!flipped);
+  }
+
   const movieCardID = `movie-${movie.id}`
   return (
-    <article id={movieCardID} className="movie-card">
+    <article id={movieCardID} className={"movie-card" + (flipped ? " flipped" : "")}>
       <img src={movie.poster_path ? `${pathToPoster}${movie.poster_path}` : placeholderPoster} alt={movie.title} className="movie-card-poster" />
-      <div className="movie-card-overlay">
+      <div className="movie-card-overlay" onClick={flipCard}>
         <div className="movie-rating">
           <StarRating rating={movie.vote_average}/>
           <span className="score-rating .screen-reader-text">{movie.vote_average*10}%</span>
