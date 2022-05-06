c02e78cb2fa3eeb70c5bb6186b6126f605e8ca20
cljang <clayton.jang@gmail.com>
2022-05-06 16:00:52 -0700

    Add content and style about page

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { appTitle } from "../global/globals";
+import tmdbLogo from "../images/tmdb-logo.svg"
 
 const PageAbout = () => {
 
@@ -16,6 +17,14 @@ const PageAbout = () => {
   return (
       <section className="page-about">
         <h2>About</h2>
+        <div className="about-content">
+          <p><span className="app-title">ovation.</span> is an online movie database that enables users to track their favourite movies.</p>
+          <p>Browse and view detailed information about popular, top rated, upcoming, and now playing titles.</p>
+          <div className="attribution">
+            <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+            <a href="https://www.themoviedb.org/"><img src={tmdbLogo} alt="The Movie Database logo" /></a>
+          </div>
+        </div>
       </section>
   );
 
