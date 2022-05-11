e29ca668116315ed1cd925ad89b04d05bed1513e
cljang <clayton.jang@gmail.com>
2022-05-10 22:10:17 -0700

    Add filter button styling for large viewport sizes

--- src/components/MovieFilter.jsx
+++ src/components/MovieFilter.jsx
@@ -8,20 +8,33 @@ function MovieFilter() {
   const selectedMovieFilter = useSelector((state) => state.movieFilter.value);
 
   const dispatch = useDispatch()
+  // const setBackground = (element) => {
+  //   const background = document.querySelector(".movie-filter-selector .filter-background");
+  //   if (background) {
+  //     background.classList.remove("popular");
+  //     background.style.position = "absolute";
+  //     background.style.top = element.offsetTop + "px";
+  //     background.style.left = element.offsetLeft + "px";
+  //     background.style.width = element.offsetWidth + "px";
+  //     background.style.height = element.offsetHeight + "px";
+  //   }
+  // };
 
   // Function to change the movie filter state
   const handleFilterChange = (e) => {
     dispatch(setMovieFilter(e.target.value))    
+    // setBackground(e.target);
   }
   
   
   return (
     <div className="movie-filter-selector">
+      {/* <div className="filter-background popular"></div> */}
       {movieFilters.map((filter) => {
         return <button key={filter.value} 
                       value={filter.value}
                       onClick={handleFilterChange}
-                      className={"movie-filter" + (filter.value===selectedMovieFilter && " active")}>
+                      className={`movie-filter ${filter.value}` + (filter.value===selectedMovieFilter ? " active" : "")}>
                 {filter.text}
               </button>
       })}
