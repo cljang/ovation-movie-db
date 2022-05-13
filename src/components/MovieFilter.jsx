362d95fc028b5bd9be9960ef151d4449913d8171
cljang <clayton.jang@gmail.com>
2022-05-12 21:29:53 -0700

    Use separate buttons for filter selector to make it more accessible when using keyboard

--- src/components/MovieFilter.jsx
+++ src/components/MovieFilter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux"
 import { FaChevronDown as DropdownArrow } from "react-icons/fa"
 import { setMovieFilter } from "../features/movieFilter/movieFilterSlice"
@@ -8,39 +8,62 @@ function MovieFilter() {
   // Movie Filter
   const movieFilter = useSelector((state) => state.movieFilter);
   // Button Open
-  const [ buttonOpen, setButtonOpen ] = useState(false);
+  const [ selectorButtonOpen, setSelectorButtonOpen ] = useState(false);
 
   const dispatch = useDispatch()
 
-  const handleButtonClick = (e) => {
-    e.preventDefault();
-    setButtonOpen(!buttonOpen);
-  }
-
   // Handle Button Close on Blur
   const closeButton = () => {
-    setButtonOpen(false);
+    setSelectorButtonOpen(false);
   }
 
-  // Function to change the movie filter state
-  const handleFilterChange = (e) => {
+  // Function to toggle the selector button
+  const handleSelectorButtonClick = (e) => {
+    e.preventDefault();
+    setSelectorButtonOpen(!selectorButtonOpen);
+  }
+
+  // Function to change the movie filter state to match the clicked filter button
+  const handleFilterButtonClick = (e) => {
+    e.preventDefault();
     dispatch(setMovieFilter(e.target.value));
     closeButton();
   }
 
+  // Detect click outside of filter
+  // https://stackoverflow.com/questions/32553158/detect-click-outside-react-component
+  const wrapperRef = useRef(null);
+
+  const useOutsideAlerter = (ref) => {
+    useEffect(() => {
+      // Function that checks if there is a current reference and it doesn't contain the target
+      // Clicked outside the ref element
+      const handleClickOutside = (e) => {
+        if (ref.current && !ref.current.contains(e.target)) {
+          closeButton();
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref])
+  }
+  
+  useOutsideAlerter(wrapperRef)
+
   return (
-    <form className="movie-filter-selector">
-      <button onClick={handleButtonClick} onBlur={closeButton}>
+    <form className="movie-filter-selector" ref={wrapperRef}>
+      <button onClick={handleSelectorButtonClick} className="selector-btn">
         <p>{movieFilter.text}</p>
-        <DropdownArrow className={"dropdown-arrow" + (buttonOpen ? " filter-open" : "")} />
+        <DropdownArrow className={"dropdown-arrow" + (selectorButtonOpen ? " filter-open" : "")} />
       </button>
-      <fieldset className={"selectors" + (buttonOpen ? " filter-open" : "")}>
+      <fieldset className={"filters" + (selectorButtonOpen ? " filter-open" : "")}>
         {movieFilter.valid_values.map((filter) => {
           return (
-            <div key={filter.value}>
-              <input type="radio" name="filter" id={filter.value} value={filter.value} onChange={handleFilterChange}/>
-              <label htmlFor={filter.value}>{filter.text}</label>
-            </div>
+            <button key={filter.value} value={filter.value} onClick={handleFilterButtonClick} className="filter-btn">{filter.text}</button>
           )
         })}
       </fieldset>
