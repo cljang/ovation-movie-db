2a1223ca4b590fc16c75c7a8f4c4d5028e25c19e
cljang <clayton.jang@gmail.com>
2022-05-18 09:02:24 -0700

    Automatically close the nav when clicking off of it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
+import { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import NavMain from "./NavMain";
 import { useSelector, useDispatch } from "react-redux"
-import { toggleNav } from "../features/navOpen/navOpenSlice";
+import { closeNav, toggleNav } from "../features/navOpen/navOpenSlice";
 
 function Header() {
+
+  const buttonRef = useRef();
+  const navRef = useRef();
   
   const navOpen = useSelector((state) => state.navOpen.value);
   const dispatch = useDispatch();
@@ -12,6 +16,26 @@ function Header() {
     e.preventDefault();
     dispatch(toggleNav(navOpen))
   }
+
+  useEffect(() => {
+    // Function that checks if there is a current reference and it doesn't contain the target
+    // Clicked outside the ref element
+    const handleClickOutside = (e) => {
+      if (navRef.current && 
+          !navRef.current.contains(e.target) && 
+          buttonRef.current && 
+          !buttonRef.current.contains(e.target) ) 
+      {
+        dispatch(closeNav())
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dispatch, navRef, buttonRef])
 
   return (
     <header>
@@ -29,12 +53,13 @@ function Header() {
           aria-controls="header-menu"
           aria-expanded={navOpen}
           onClick={handleNavButton}
+          ref={buttonRef}
         >
           <span className="bar" id="bar-1"></span>
           <span className="bar" id="bar-2"></span>
           <span className="bar" id="bar-3"></span>
         </button>
-        <NavMain />
+        <NavMain reference={navRef}/>
       </div>
     </header>
   );
--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -4,7 +4,7 @@ import { navMainLinks } from "../global/globals";
 import { closeNav } from "../features/navOpen/navOpenSlice"
 import SearchBar from "./SearchBar";
 
-function NavMain() {
+function NavMain({ reference }) {
 
   const navOpen = useSelector((state) => state.navOpen.value);
   
@@ -17,7 +17,7 @@ function NavMain() {
   return (
     <div className={"navbar-menu" + (navOpen ? " navbar-toggled" : "")}>
       <SearchBar />
-      <nav>
+      <nav ref={reference}>
         <ul>
           {navMainLinks.map(link => {
             return(
