7c1ee3e8552c1dd7030781ab98ba7a4b14b2168e
cljang <clayton.jang@gmail.com>
2022-05-22 22:00:57 -0700

    Rebuild movie carousel using slick instead of swiper

--- src/components/MovieCarousel.jsx
+++ src/components/MovieCarousel.jsx
@@ -1,8 +1,7 @@
-
-import 'swiper/css/bundle';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "../scss/components/_movieCarousel.scss"
-import { Swiper, SwiperSlide } from "swiper/react"
-import { Pagination, Autoplay, Lazy } from "swiper";
+import Slider from "react-slick";
 import { pathToBackdrop780, pathToBackdrop1280, pathToOriginalImage } from "../global/globals";
 import { useDispatch } from "react-redux"
 import { closeNav } from "../features/navOpen/navOpenSlice"
@@ -13,18 +12,12 @@ function MovieCarousel({movieList}) {
   const dispatch = useDispatch()
 
   const settings = {
-    // spaceBetween: 10,
-    slidesPerView: 'auto',
-    loop: true,
-    modules:[ Pagination, Autoplay, Lazy ],
-    pagination:{
-      clickable: true,
-    },
-    autoplay:{
-      delay: 5000,
-      pauseOnMouseEnter: true,
-    },
-    // lazy: true,
+    dots: true,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    lazyLoad: 'progressive',
   }
 
   // Add a special exception when clicking the carousel to close the navMenu
@@ -35,40 +28,31 @@ function MovieCarousel({movieList}) {
 
   return (
     movieList && movieList.length > 0 &&
-      <Swiper {...settings} className="movie-carousel" onClick={handleSlideClick}>
+      <Slider {...settings} className="movie-carousel" onClick={handleSlideClick}>
         {movieList.map((movie => {
           return (
-            <SwiperSlide key={movie.id} className="carousel-slide">
-              <div className='movie-backdrop'>
-              <>
-                {/* {movie.backdrop_path && 
+            <div key={movie.id} className="carousel-slide">
+              {movie.backdrop_path && 
+                <div className="movie-backdrop">
                   <picture>
-                    <source media='(max-width: 400px)' srcSet={movie.backdrop_path ? `${pathToBackdrop780}${movie.backdrop_path}` : ""} />
-                    <source media='(max-width: 720px)' srcSet={movie.backdrop_path ? `${pathToBackdrop1280}${movie.backdrop_path}` : ""} />
-                    <img 
-                      src={`${pathToOriginalImage}${movie.backdrop_path}`} 
-                      alt={`${movie.title} backdrop`} 
-                      className="swiper-lazy"
-                    />
+                    <source media="(max-width: 400px)" srcSet={`${pathToBackdrop780}${movie.backdrop_path}`}/>
+                    <source media="(max-width: 720px)" srcSet={`${pathToBackdrop1280}${movie.backdrop_path}`}/>
+                    <img src={`${pathToOriginalImage}${movie.backdrop_path}`} alt="" className="backdrop-image" />
                   </picture>
-                } */}
-                {movie.backdrop_path && 
-                  <img 
-                    src={`${pathToOriginalImage}${movie.backdrop_path}`} 
-                    alt={`${movie.title} backdrop`} 
-                  />
-                }
-              </>
+                </div>
+              }
+              
+              <div className="carousel-content">
+                <div className='carousel-text'>
+                  <h3 className='movie-title'>{movie.title}</h3>
+                  <p className='movie-overview'>{movie.overview}</p>
+                  <MovieInfoButton movie={movie} tabIndex={-1}/>
+                </div>
               </div>
-              <div className='carousel-text'>
-                <h3 className='movie-title'>{movie.title}</h3>
-                <p className='movie-overview'>{movie.overview}</p>
-                <MovieInfoButton movie={movie} tabIndex={-1}/>
-              </div>
-            </SwiperSlide>
+            </div>
           )
         }))}
-      </Swiper>
+      </Slider>
     
   )
 }
